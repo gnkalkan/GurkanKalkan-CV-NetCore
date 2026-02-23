363f14717b3d3bf90270308b4180d1cb9588917a
Gürkan N. Kalkan <30825085+nesimigurkankalkan@users.noreply.github.com>
2026-02-23 21:32:57 +0300

    Added SQL Lite

---
+++ Business/IOC.cs
@@ -0,0 +1,21 @@
+﻿using Core.Abstracts;
+using Data;
+using Data.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Business
+{
+    // IOC (Inversion of Control)
+    public static class IOC 
+    {
+        public static IServiceCollection AddDataAccessDependencies(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<CvDbContext>(options => options.UseSqlite(configuration.GetConnectionString("cvdb")));
+
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            return services;
+        }
+    }
+}
