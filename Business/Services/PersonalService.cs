c09ceb49662e9e3bfafd9adef660d7afff9250a0
Gürkan N. Kalkan <30825085+nesimigurkankalkan@users.noreply.github.com>
2026-02-24 16:29:27 +0300

    Update All

--- Business/Services/PersonalService.cs
+++ Business/Services/PersonalService.cs
@@ -1,14 +1,59 @@
 ﻿using AutoMapper;
 using Core.Abstracts;
 using Core.Abstracts.IServices;
-using Microsoft.EntityFrameworkCore.Metadata;
-using System;
-using System.Collections.Generic;
-using System.Text;
+using Core.Concretes.DTOs;
+using Utilities.Constants;
+using Utilities.Results;
 
 namespace Business.Services
 {
-    public class PersonalService(IUnitOfWork uniOfWork, IMapper mapper) : IPersonalService
+    public class PersonalService(IUnitOfWork unitOfWork, IMapper mapper) : IPersonalService
     {
+        public Task<IResult> AddAsync(PersonalDTO personalDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IResult> DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IDataResult<IEnumerable<PersonalDTO>>> GetAllAsync()
+        {
+            try
+            {
+                var personals = await unitOfWork.PersonalRepository.FindManyAsync();
+                var personalDtos = mapper.Map<IEnumerable<PersonalDTO>>(personals);
+                return new SuccessDataResult<IEnumerable<PersonalDTO>>(personalDtos, $"Personals {Messages.RetrievedSuffix}");
+            }
+            catch (Exception ex)
+            {
+                return new ErrorDataResult<IEnumerable<PersonalDTO>>($"{Messages.ErrorOccured}: {ex.Message}");
+            }
+        }
+
+        public async Task<IDataResult<PersonalDTO>> GetByIdAsync(int id)
+        {
+            try
+            {
+                var personal = await unitOfWork.PersonalRepository.FindByIdAsync(id);
+                if (personal == null)
+                {
+                    return new ErrorDataResult<PersonalDTO>($"Personal {Messages.NotFoundSuffix}");
+                }
+                var personalDto = mapper.Map<PersonalDTO>(personal);
+                return new SuccessDataResult<PersonalDTO>(personalDto, $"Personal {Messages.RetrievedSuffix}");
+            }
+            catch (Exception ex)
+            {
+                return new ErrorDataResult<PersonalDTO>($"{Messages.ErrorOccured}: {ex.Message}");
+            }
+        }
+
+        public Task<IResult> UpdateAsync(PersonalDTO personalDto)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- Utilities/Results/IDataResult.cs
+++ Utilities/Results/IDataResult.cs
@@ -21,8 +21,8 @@ namespace Utilities.Results
     }
     public class ErrorDataResult<TData> : DataResult<TData>
     {
+        public ErrorDataResult(string message) : base(default, false, message) { }
         public ErrorDataResult(TData data, string message) : base(data, false, message) { }
-        public ErrorDataResult(TData data) : base(data, false) { }
     }
 }
 
