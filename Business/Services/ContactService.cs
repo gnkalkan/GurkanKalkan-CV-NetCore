ea12b2a278007b91c65c21033cdb3a6aecdaadd8
Gürkan N. Kalkan <30825085+nesimigurkankalkan@users.noreply.github.com>
2026-02-28 14:15:30 +0300

    Update ContactService

--- Business/Services/ContactService.cs
+++ Business/Services/ContactService.cs
@@ -2,35 +2,95 @@
 using Core.Abstracts;
 using Core.Abstracts.IServices;
 using Core.Concretes.DTOs;
+using Core.Concretes.Entities;
+using Utilities.Constants;
 using Utilities.Results;
 
 namespace Business.Services
 {
     public class ContactService(IUnitOfWork unitOfWork, IMapper mapper) : IContactService
     {
-        public Task<IResult> AddAsync(AddContactDTO dto)
+        public async Task<IResult> AddAsync(AddContactDTO dto)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var contact = mapper.Map<Contact>(dto);
+
+                await unitOfWork.ContactRepository.CreateAsync(contact);
+                await unitOfWork.CommitAsync();
+                return new SuccessResult("Contact" + Messages.AddedSuffix);
+            }
+            catch (Exception ex)
+            {
+                return new ErrorResult(Messages.ErrorOccured + " : " + ex.Message);
+            }
         }
 
-        public Task<IResult> DeleteAsync(int id)
+        public async Task<IResult> DeleteAsync(int id)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var contact = await unitOfWork.ContactRepository.FindByIdAsync(id);
+                if (contact == null)
+                    return new ErrorResult("Contact" + Messages.NotFoundSuffix);
+                await unitOfWork.ContactRepository.DeleteAsync(contact);
+                await unitOfWork.CommitAsync();
+                return new SuccessResult("Contact" + Messages.DeletedSuffix);
+            }
+            catch (Exception ex)
+            {
+
+                return new ErrorResult(Messages.ErrorOccured + " : " + ex.Message);
+            }
         }
 
-        public Task<IDataResult<IEnumerable<ContactListDTO>>> GetAllAsync()
+        public async Task<IDataResult<IEnumerable<ContactListDTO>>> GetAllAsync()
         {
-            throw new NotImplementedException();
+            try
+            {
+                var contacts = await unitOfWork.ContactRepository.FindManyAsync();
+                var contactDTOs = mapper.Map<IEnumerable<ContactListDTO>>(contacts.ToList());
+                return new SuccessDataResult<IEnumerable<ContactListDTO>>(contactDTOs, "Contacts" + Messages.RetrievedSuffix);
+            }
+            catch (Exception ex)
+            {
+                return new ErrorDataResult<IEnumerable<ContactListDTO>>(Messages.ErrorOccured + ": " + ex.Message);
+            }
         }
 
-        public Task<IDataResult<ContactListDTO>> GetByIdAsync(int id)
+        public async Task<IDataResult<ContactListDTO>> GetByIdAsync(int id)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var contact = await unitOfWork.ContactRepository.FindByIdAsync(id);
+                if (contact == null)
+                    return new ErrorDataResult<ContactListDTO>("Contact" + Messages.NotFoundSuffix);
+                var contactDTO = mapper.Map<ContactListDTO>(contact);
+                return new SuccessDataResult<ContactListDTO>(contactDTO, "Contact" + Messages.RetrievedSuffix);
+            }
+            catch (Exception ex)
+            {
+                return new ErrorDataResult<ContactListDTO>(Messages.ErrorOccured + ": " + ex.Message);
+            }
         }
 
-        public Task<IResult> UpdateAsync(UpdateContactDTO dto)
+        public async Task<IResult> UpdateAsync(UpdateContactDTO dto)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var existingContact = await unitOfWork.ContactRepository.FindByIdAsync(dto.Id);
+                if (existingContact == null)
+                    return new ErrorResult("Contact" + Messages.NotFoundSuffix);
+
+                mapper.Map(dto, existingContact);
+                await unitOfWork.ContactRepository.UpdateAsync(existingContact);
+                await unitOfWork.CommitAsync();
+                return new SuccessResult("Contact" + Messages.UpdatedSuffix);
+            }
+            catch (Exception ex)
+            {
+                return new ErrorResult(Messages.ErrorOccured + " : " + ex.Message);
+            }
         }
     }
 }
--- Core/Concretes/Entities/Skill.cs
+++ Core/Concretes/Entities/Skill.cs
@@ -7,8 +7,7 @@ namespace Core.Concretes.Entities
         public required string Title { get; set; } = null!;
         //public byte Proficiency { get; set; }
 
-
-
+        #region
         // One-to-Many (Bire Çok)
         //public int ProjectId { get; set; }
         //public virtual Project Project { get; set; }
@@ -16,5 +15,6 @@ namespace Core.Concretes.Entities
         //Navigation Properties
         //Many-to-Many (Çoka Çok)
         //public virtual ICollection<Project> Projects { get; set; } = [];
+        #endregion
     }
 }
