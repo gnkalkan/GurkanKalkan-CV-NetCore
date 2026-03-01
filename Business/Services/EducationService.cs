436179d48cb9652821fd191bc6a9814460fd09fe
Gürkan N. Kalkan <30825085+nesimigurkankalkan@users.noreply.github.com>
2026-03-01 19:35:41 +0300

    Added EducationService Codes

--- Business/Services/EducationService.cs
+++ Business/Services/EducationService.cs
@@ -2,35 +2,95 @@
 using Core.Abstracts;
 using Core.Abstracts.IServices;
 using Core.Concretes.DTOs;
+using Core.Concretes.Entities;
+using Utilities.Constants;
 using Utilities.Results;
 
 namespace Business.Services
 {
     public class EducationService(IUnitOfWork unitOfWork, IMapper mapper) : IEducationService
     {
-        public Task<IResult> AddAsync(AddEducationDTO dto)
+        public async Task<IResult> AddAsync(AddEducationDTO dto)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var education = mapper.Map<Education>(dto);
+
+                await unitOfWork.EducationRepository.CreateAsync(education);
+                await unitOfWork.CommitAsync();
+                return new SuccessResult("Education" + Messages.AddedSuffix);
+            }
+            catch (Exception ex)
+            {
+                return new ErrorResult(Messages.ErrorOccured + " : " + ex.Message);
+            }
         }
 
-        public Task<IResult> DeleteAsync(int id)
+        public async Task<IResult> DeleteAsync(int id)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var education = await unitOfWork.EducationRepository.FindByIdAsync(id);
+                if (education == null)
+                    return new ErrorResult("Education" + Messages.NotFoundSuffix);
+                await unitOfWork.EducationRepository.DeleteAsync(education);
+                await unitOfWork.CommitAsync();
+                return new SuccessResult("Education" + Messages.DeletedSuffix);
+            }
+            catch (Exception ex)
+            {
+
+                return new ErrorResult(Messages.ErrorOccured + " : " + ex.Message);
+            }
         }
 
-        public Task<IDataResult<IEnumerable<EducationListDTO>>> GetAllAsync()
+        public async Task<IDataResult<IEnumerable<EducationListDTO>>> GetAllAsync()
         {
-            throw new NotImplementedException();
+            try
+            {
+                var educations = await unitOfWork.EducationRepository.FindManyAsync();
+                var educationDTOs = mapper.Map<IEnumerable<EducationListDTO>>(educations.ToList());
+                return new SuccessDataResult<IEnumerable<EducationListDTO>>(educationDTOs, "Educations" + Messages.RetrievedSuffix);
+            }
+            catch (Exception ex)
+            {
+                return new ErrorDataResult<IEnumerable<EducationListDTO>>(Messages.ErrorOccured + ": " + ex.Message);
+            }
         }
 
-        public Task<IDataResult<EducationListDTO>> GetByIdAsync(int id)
+        public async Task<IDataResult<EducationListDTO>> GetByIdAsync(int id)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var education = await unitOfWork.EducationRepository.FindByIdAsync(id);
+                if (education == null)
+                    return new ErrorDataResult<EducationListDTO>("Education" + Messages.NotFoundSuffix);
+                var educationDTO = mapper.Map<EducationListDTO>(education);
+                return new SuccessDataResult<EducationListDTO>(educationDTO, "Education" + Messages.RetrievedSuffix);
+            }
+            catch (Exception ex)
+            {
+                return new ErrorDataResult<EducationListDTO>(Messages.ErrorOccured + ": " + ex.Message);
+            }
         }
 
-        public Task<IResult> UpdateAsync(UpdateEducationDTO dto)
+        public async Task<IResult> UpdateAsync(UpdateEducationDTO dto)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var existingEducation = await unitOfWork.EducationRepository.FindByIdAsync(dto.Id);
+                if (existingEducation == null)
+                    return new ErrorResult("Education" + Messages.NotFoundSuffix);
+
+                mapper.Map(dto, existingEducation);
+                await unitOfWork.EducationRepository.UpdateAsync(existingEducation);
+                await unitOfWork.CommitAsync();
+                return new SuccessResult("Education" + Messages.UpdatedSuffix);
+            }
+            catch (Exception ex)
+            {
+                return new ErrorResult(Messages.ErrorOccured + " : " + ex.Message);
+            }
         }
     }
 }
